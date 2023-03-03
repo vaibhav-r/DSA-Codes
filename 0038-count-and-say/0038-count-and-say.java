c669186af27f3f80592b300eadaa54940fbc1200
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-03-03 22:01:40 +0530

    Time: 2 ms (98.53%), Space: 40.2 MB (62.39%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1) return "1";
+        String prev = countAndSay(n-1);
+        StringBuilder sb = new StringBuilder();
+        char c = prev.charAt(0);
+        int count = 1;
+        for(int i=1; i<prev.length(); i++){
+            if(prev.charAt(i) == c) count++;
+            else{
+                sb.append(count).append(c);
+                c = prev.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count).append(c);
+        return sb.toString();
+    }
+}
