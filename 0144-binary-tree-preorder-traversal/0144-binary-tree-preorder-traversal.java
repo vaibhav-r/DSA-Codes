b1389657d41175763d9573fea6a0e2dba37f8d46
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-03-06 17:20:00 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (41.19%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,38 +14,38 @@
  * }
  */
 // Recursive Method:
-// class Solution {
-//     public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> res=new ArrayList<>();
-//         helper(root,res);
-//         return res;
-//     }
-//     public void helper(TreeNode root,List<Integer> res){
-//         if(root!=null){
-//             res.add(root.val);
-//             helper(root.left,res);
-//             helper(root.right,res);
-//         }
-//     }
-// }
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+    public void helper(TreeNode root,List<Integer> res){
+        if(root!=null){
+            res.add(root.val);
+            helper(root.left,res);
+            helper(root.right,res);
+        }
+    }
+}
 
 // OR
 
 // Iterative Method:
-class  Solution{
-    public List<Integer> preorderTraversal(TreeNode root){
-        List<Integer> ans=new ArrayList<>();
-        Stack<TreeNode> st=new Stack<>();
-        st.add(root);
-        while(!st.isEmpty()){
-            TreeNode curr=st.peek();
-            st.pop();
-            if(curr!=null){
-                ans.add(curr.val);
-                st.add(curr.right);
-                st.add(curr.left);
-            }
-        }
-        return ans;
-    }
-}
+// class  Solution{
+//     public List<Integer> preorderTraversal(TreeNode root){
+//         List<Integer> ans=new ArrayList<>();
+//         Stack<TreeNode> st=new Stack<>();
+//         st.add(root);
+//         while(!st.isEmpty()){
+//             TreeNode curr=st.peek();
+//             st.pop();
+//             if(curr!=null){
+//                 ans.add(curr.val);
+//                 st.add(curr.right);
+//                 st.add(curr.left);
+//             }
+//         }
+//         return ans;
+//     }
+// }
