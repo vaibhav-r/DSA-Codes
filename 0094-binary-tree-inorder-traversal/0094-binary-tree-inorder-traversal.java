a3bfc830ae7061139e1f01d04ccabe13d9716107
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-03-07 20:37:39 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (30.06%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,21 +13,52 @@
  *     }
  * }
  */
-//Recurrsive Method:
+//Morris Method:
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res=new ArrayList<>();
-        helper(root,res);
-        return res;
-    }
-    public void helper(TreeNode root,List<Integer> res){
-        if(root!=null){
-            helper(root.left,res);
-            res.add(root.val);
-            helper(root.right,res);
+        List<Integer> inorder = new ArrayList<Integer>(); 
+        
+        TreeNode cur = root; 
+        while(cur != null) {
+            if(cur.left == null) {
+                inorder.add(cur.val); 
+                cur = cur.right; 
+            }
+            else {
+                TreeNode prev = cur.left; 
+                while(prev.right != null && prev.right != cur) {
+                    prev = prev.right; 
+                }
+                
+                if(prev.right == null) {
+                    prev.right = cur;
+                    cur = cur.left; 
+                }
+                else {
+                    prev.right = null; 
+                    inorder.add(cur.val); 
+                    cur = cur.right; 
+                }
+            }
         }
+        return inorder; 
     }
 }
+//Recursive Method:
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res=new ArrayList<>();
+//         helper(root,res);
+//         return res;
+//     }
+//     public void helper(TreeNode root,List<Integer> res){
+//         if(root!=null){
+//             helper(root.left,res);
+//             res.add(root.val);
+//             helper(root.right,res);
+//         }
+//     }
+// }
 
 // OR
 
