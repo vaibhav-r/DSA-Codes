5c8ace3fd50f18a8f5ee18b722b611dd9d04d4b9
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-04-16 14:04:57 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -45,31 +45,26 @@ class GFG {
 
 
 class Solution {
-    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
-        if(m > n) {
-            return kthElement(arr2, arr1, m, n, k); 
-        }
-            
-        int low = Math.max(0,k-m), high = Math.min(k,n);
-            
-        while(low <= high) {
-            int cut1 = (low + high) >> 1; 
-            int cut2 = k - cut1; 
-            int l1 = cut1 == 0 ? Integer.MIN_VALUE : arr1[cut1 - 1]; 
-            int l2 = cut2 == 0 ? Integer.MIN_VALUE : arr2[cut2 - 1];
-            int r1 = cut1 == n ? Integer.MAX_VALUE : arr1[cut1]; 
-            int r2 = cut2 == m ? Integer.MAX_VALUE : arr2[cut2]; 
-                
-            if(l1 <= r2 && l2 <= r1) {
-                return Math.max(l1, l2);
+    public long kthElement( int array1[], int array2[], int n, int m, int k) {
+        int p1 = 0, p2 = 0, counter = 0, answer = 0;
+
+        while (p1 < n && p2 < m) {
+            if (counter == k) break;
+            else if (array1[p1] < array2[p2]) {
+                answer = array1[p1];
+                ++p1;
+            } else {
+                answer = array2[p2];
+                ++p2;
             }
-            else if (l1 > r2) {
-                high = cut1 - 1;
-            }
-            else {
-                low = cut1 + 1; 
-            }
+            ++counter;
         }
-        return -1;
+        if (counter != k) {
+            if (p1 < n)
+                answer = array1[p1 + k - counter - 1];
+            else
+                answer = array2[p2 + k - counter - 1];
         }
+        return answer;
+    }
 }
