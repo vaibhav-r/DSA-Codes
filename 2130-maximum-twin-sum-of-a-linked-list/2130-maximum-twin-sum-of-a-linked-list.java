7ca19c43ec2625b959733aa89f34ed5039472197
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-05-17 22:04:43 +0530

    Time: 5 ms (89.63%), Space: 62.3 MB (52.85%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -8,46 +8,47 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public int pairSum(ListNode head) {
-//         ListNode slow=head,fast=head;
-//         while(fast!=null && fast.next!=null){
-//             slow=slow.next;
-//             fast=fast.next.next;
-//         }
-//         ListNode prev=null,NextNode=null;
-//         while(slow!=null){
-//             NextNode=slow.next;
-//             slow.next=prev;
-//             prev=slow;
-//             slow=NextNode;
-//         }
-//         int maxi=0;
-//         slow=head;
-//         while(prev!=null){
-//             maxi=Math.max(maxi,(slow.val+prev.val));
-//             prev=prev.next;
-//             slow=slow.next;
-//         }
-//         return maxi;
-//     }
-// }
 class Solution {
     public int pairSum(ListNode head) {
-        ListNode cur=head;
-        Stack<Integer> st=new Stack<>();
-        while(cur!=null){
-            st.push(cur.val);
-            cur=cur.next;
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        cur=head;
-        int maxi=0,count=st.size(),i=1;
-        while(i<=count/2){
-            maxi=Math.max(maxi,(cur.val+st.peek()));
-            cur=cur.next;
-            st.pop();
-            i++;
+        ListNode prev=null,NextNode=null;
+        while(slow!=null){
+            NextNode=slow.next;
+            slow.next=prev;
+            prev=slow;
+            slow=NextNode;
+        }
+        int maxi=0;
+        slow=head;
+        while(prev!=null){
+            maxi=Math.max(maxi,(slow.val+prev.val));
+            prev=prev.next;
+            slow=slow.next;
         }
         return maxi;
     }
 }
+// OR
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         ListNode cur=head;
+//         Stack<Integer> st=new Stack<>();
+//         while(cur!=null){
+//             st.push(cur.val);
+//             cur=cur.next;
+//         }
+//         cur=head;
+//         int maxi=0,count=st.size(),i=1;
+//         while(i<=count/2){
+//             maxi=Math.max(maxi,(cur.val+st.peek()));
+//             cur=cur.next;
+//             st.pop();
+//             i++;
+//         }
+//         return maxi;
+//     }
+// }
