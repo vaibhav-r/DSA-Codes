67f095198c54dd4869638973ad0b6defad40ee2a
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-07-01 08:51:28 +0530

    Time: 1 ms (95.89%), Space: 39.1 MB (87.29%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,11 +1,24 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         int cnt=0;
-        for(int i=1;i*2<=num;i++){
-            if(num%i==0) cnt+=i;
-            if (cnt>num) return false;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0){
+                cnt+=i;
+                if(i*i!=num) cnt+=num/i;
+            }
         }
-        return cnt==num;
+        return cnt-num==num;
     }
-    
 }
+// OR
+// class Solution {
+//     public boolean checkPerfectNumber(int num) {
+//         int cnt=0;
+//         for(int i=1;i*2<=num;i++){
+//             if(num%i==0) cnt+=i;
+//             if (cnt>num) return false;
+//         }
+//         return cnt==num;
+//     }
+    
+// }
