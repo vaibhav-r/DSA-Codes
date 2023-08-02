1290ed0e9e7f646072fd7af89d1fc21725db3456
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-08-02 22:57:47 +0530

    Time: 1 ms (98.58%), Space: 44 MB (63.60%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        funcPermu(0,nums,ans);
+        return ans;
+    }
+    public void funcPermu(int ind,int[] nums,List<List<Integer>> ans){
+        if(ind==nums.length){
+            List<Integer> ds= new ArrayList<>();
+            for(int i=0; i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            ans.add(ds);
+            return;
+        }
+        for(int i=ind;i<nums.length;i++){
+            swap(i,ind,nums);
+            funcPermu(ind+1,nums,ans);
+            swap(i,ind,nums);
+        }
+    }
+    public void swap(int i, int j, int[] nums){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+}
