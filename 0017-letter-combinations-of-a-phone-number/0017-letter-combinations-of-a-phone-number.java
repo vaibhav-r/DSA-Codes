f67871d99e2587753d45fa29649a3340200e32b2
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-08-03 22:50:18 +0530

    Time: 1 ms (78.73%), Space: 41.2 MB (64.44%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        Map<Character, String> phoneMap = new HashMap<>();
+        phoneMap.put('2', "abc");
+        phoneMap.put('3', "def");
+        phoneMap.put('4', "ghi");
+        phoneMap.put('5', "jkl");
+        phoneMap.put('6', "mno");
+        phoneMap.put('7', "pqrs");
+        phoneMap.put('8', "tuv");
+        phoneMap.put('9', "wxyz");
+
+        backtrack(result, phoneMap, digits, new StringBuilder(), 0);
+
+        return result;
+    }
+
+    private void backtrack(List<String> result, Map<Character, String> phoneMap, String digits, StringBuilder current, int index) {
+        if (index == digits.length()) {
+            result.add(current.toString());
+            return;
+        }
+
+        char digit = digits.charAt(index);
+        String letters = phoneMap.get(digit);
+        for (char letter : letters.toCharArray()) {
+            current.append(letter);
+            backtrack(result, phoneMap, digits, current, index + 1);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
