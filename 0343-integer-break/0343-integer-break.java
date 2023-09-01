b3d4a5fca11369e02621c43b37a7c92ca703d270
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-09-01 22:59:59 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (62.61%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n<=3){
+            return n-1;
+        }
+        int[] dp=new int[n+1];
+        dp[2]=2;
+        dp[3]=3;
+        for(int i=4;i<=n;i++){
+            dp[i]=Math.max(dp[i-2]*2,dp[i-3]*3);
+        }
+        return dp[n];
+    }
+}
