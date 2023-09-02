8885f0353b78cd68088e19af243e394ef7470192
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-09-02 17:05:36 +0530

    Time: 14 ms (59.44%), Space: 47.7 MB (51.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,28 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Count the frequency of each element
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        int[] x = new int[k];
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        for (int i : nums) {
+            h.put(i, h.getOrDefault(i, 0) + 1);
         }
 
-        // Use a min heap to maintain the top k frequent elements
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> frequencyMap.get(a) - frequencyMap.get(b));
+        // Create a list to hold the entries of the HashMap
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(h.entrySet());
 
-        for (int num : frequencyMap.keySet()) {
-            minHeap.add(num);
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            }
+        // Sort the list based on the values (frequencies) in descending order
+        Collections.sort(list, (a, b) -> b.getValue().compareTo(a.getValue()));
+
+        for (int i = 0; i < k; i++) {
+            x[i] = list.get(i).getKey();
         }
 
-        // Build the result array from the min heap
-        int[] result = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            result[i] = minHeap.poll();
-        }
-
-        return result;
+        return x;
     }
 }
