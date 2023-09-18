770527c69d4237c1e1e2b91cdd39fe724764db74
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-09-18 22:16:45 +0530

    Time: 1 ms (99.31%), Space: 44.7 MB (42.52%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] strength = new int[m];
+        
+        for (int i = 0; i < m; i++) {
+            strength[i] = countSoldiers(mat[i]);
+        }
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+            (a, b) -> strength[a] == strength[b] ? a - b : strength[a] - strength[b]
+        );
+        
+        for (int i = 0; i < m; i++) {
+            minHeap.offer(i);
+        }
+        
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = minHeap.poll();
+        }
+        
+        return result;
+    }
+    
+    private int countSoldiers(int[] row) {
+        int left = 0;
+        int right = row.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == 1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return left;
+    }
+}
