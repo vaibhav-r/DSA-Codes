9eaf59633d8719117d51d078e29b49a231783790
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-09-28 23:56:11 +0530

    Update 0081-search-in-rotated-sorted-array-ii.java

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean search(int[] nums, int target) {
         Arrays.sort(nums);
         int lo=0,hi=nums.length-1;
-        while(lo<=hi){
+        while(lo<=hi) {
             int mid=(lo+hi)/2;
             if(nums[mid]==target) return true;
             else if(nums[mid]>target) hi=mid-1;
