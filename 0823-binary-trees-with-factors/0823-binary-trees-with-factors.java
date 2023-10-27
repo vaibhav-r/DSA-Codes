b23fea3851bbd45048135e481f46e4c7cd0fccf6
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-10-27 23:33:16 +0530

    Time: 18 ms (80.00%), Space: 43 MB (68.89%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numFactoredBinaryTrees(int[] arr) {
+        int MOD = 1_000_000_007;
+        Arrays.sort(arr);
+        long[] dp = new long[arr.length];
+        Arrays.fill(dp, 1);
+
+        Map<Integer, Integer> index = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            index.put(arr[i], i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] % arr[j] == 0) {
+                    int complement = arr[i] / arr[j];
+                    if (index.containsKey(complement)) {
+                        dp[i] = (dp[i] + dp[j] * dp[index.get(complement)]) % MOD;
+                    }
+                }
+            }
+        }
+
+        long total = 0;
+        for (long numTrees : dp) {
+            total = (total + numTrees) % MOD;
+        }
+
+        return (int) total;
+    }
+
+}
