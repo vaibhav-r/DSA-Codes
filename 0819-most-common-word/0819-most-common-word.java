fdcf8152a74e0708c147fd8aef93fca2fd0d6473
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2023-12-11 22:51:04 +0530

    Update 0819-most-common-word.java

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { 
     public String mostCommonWord(String paragraph, String[] banned) {
                 paragraph = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
 
