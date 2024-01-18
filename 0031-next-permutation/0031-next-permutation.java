240026c4437e11b341a57ace8ed12a0e0a97ab15
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2024-01-18 23:59:18 +0530

    Update 0031-next-permutation.java

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,7 +3,7 @@ class Solution{
         int i = nums.length - 2;
         while(i>=0 && nums[i+1]<=nums[i] ){
             i--;
-        }
+        } 
         if(i>=0){
             int j = nums.length-1;
             while(nums[j]<=nums[i]){
