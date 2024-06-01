a6175155338be1907f6f707f274ead97b9d18fb0
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2024-06-01 19:07:15 +0530

    Time: 25 ms (52.27%), Space: 45 MB (40.21%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,28 +1,38 @@
-class Solution {
+public class Solution {
+
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;
+        int n = v.length;
+        // find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
+
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        int n = v.length;
+        // find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(v[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+
     public int minEatingSpeed(int[] piles, int h) {
-        int low=0, high=maximumm(piles),hr=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            hr=speed(piles,mid);
-            if(hr<=h) high=mid-1;
-            else low=mid+1;
+        int low = 1, high = findMax(piles);
+
+        // apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        return low;   
-    }
-    
-    public int maximumm(int[] piles){
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<piles.length;i++){
-            max=Math.max(max,piles[i]);
-        }
-        return max;
-    }
-    
-    public int speed(int[] piles,int mid){
-        int ans=0;
-        for(int i=0;i<piles.length;i++){
-            ans+=Math.ceil((double)piles[i]/(double)mid);
-        }
-        return ans;
+        return low;
     }
 }
