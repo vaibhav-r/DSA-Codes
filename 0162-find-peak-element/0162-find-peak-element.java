382eb77a809fdeb52b2c6c369e8e05c6bee65eb5
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2024-06-01 18:09:19 +0530

    Time: 0 ms (100.00%), Space: 42 MB (75.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -8,7 +8,7 @@ class Solution {
             int mid=(low+high)/2;
             if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
                 return mid;
-            }if(nums[mid]<nums[mid+1])low=mid+1;
+            }if(nums[mid]>nums[mid-1])low=mid+1;
             else high=mid-1;
         }
         return -1;
