58d9216f7e0b5f6f9c99f71b2fe1030d6936e484
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2024-08-04 23:55:43 +0530

    Time: 201 ms (12.16%), Space: 69.7 MB (29.28%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,19 +1,30 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        int ans=0,s=0;
-        int[] sum=new int[n*(n+1)/2];
-        for(int i=0;i<n;i++){
-            int temp=0;
-            for(int j=i;j<n;j++){
-                temp+=nums[j];
-                sum[s++]=temp;
+int mod = 1_000_000_007;
+
+        // A min-heap to store subarray sums
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        // Compute all subarray sums and add them to the min-heap
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += nums[j];
+                minHeap.add(currentSum);
             }
         }
-        Arrays.sort(sum);
-        int mod=1_000_000_007;
-        for(int i=left-1;i<right;i++){
-            ans=(ans+sum[i])%mod;
+        
+        // Remove the first (left-1) elements from the min-heap
+        for (int i = 1; i < left; i++) {
+            minHeap.poll();
         }
-        return (int)ans;
+        
+        // Sum the elements from index left to right
+        long result = 0;
+        for (int i = left; i <= right; i++) {
+            result = (result + minHeap.poll()) % mod;
+        }
+        
+        return (int) result;
     }
 }
